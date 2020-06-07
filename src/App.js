b2b17083e0c9b3043a53e92499b54a64d0c5e45b
Hamza Farooq <hamza3366123@gmail.com>
2020-06-07 16:03:56 +0500

    error correction

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 style="text-align: center;">Hello, World!</h1>
+      <h1>Hello, World!</h1>
       <p>Welcome to code with React using Workflow</p>
 
     </div>
